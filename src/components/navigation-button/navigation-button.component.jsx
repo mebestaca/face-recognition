e0e7624ce3e405581dd85d3c805260493faa3a53
Marc Edison Estaca <mebestaca@gmail.com>
2023-11-16 05:57:17 -0700

    added navigation button

---
+++ src/components/navigation-button/navigation-button.component.jsx
@@ -0,0 +1,7 @@
+const NavigationButton = ({caption}) => {
+    return (
+        <p className='underline pa2 ma3 link dim pointer f3 grow'>{caption}</p>
+    );
+}
+
+export default NavigationButton;
