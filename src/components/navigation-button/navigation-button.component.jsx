2511f8dee2614cf4a888f39625622e1875147b69
Marc Edison Estaca <mebestaca@gmail.com>
2023-12-07 03:03:18 -0700

    converted navigation-button to jsx

--- src/components/navigation-button/navigation-button.component.jsx
+++ src/components/navigation-button/navigation-button.component.jsx
@@ -1,6 +1,8 @@
-const NavigationButton = ({caption}) => {
+const NavigationButton = ({caption, onclick}) => {
     return (
-        <p className='underline pa2 ma3 link dim pointer f3 grow'>{caption}</p>
+        <p className='underline pa2 ma3 link dim pointer f3 grow'
+            onClick={onclick}
+        >{caption}</p>
     );
 }
 
