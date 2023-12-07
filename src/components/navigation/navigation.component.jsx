df13199cf4b0a3ce446362730d25fc3f6402810f
Marc Edison Estaca <mebestaca@gmail.com>
2023-12-07 03:07:59 -0700

    converted navigation component to jsx

--- src/components/navigation/navigation.component.jsx
+++ src/components/navigation/navigation.component.jsx
@@ -1,21 +1,38 @@
 import { Fragment } from "react"
 import NavigationButton from "../navigation-button/navigation-button.component";
 import { Outlet } from "react-router-dom";
+import { useContext } from "react";
+import { UserContext } from "../../context/user/user.context";
+import { ImageContext } from "../../context/image.context.jsx/image.context";
+import { useNavigate } from "react-router-dom";
 
 const Navigation = () => {
-    const isSignedIn = false;
+    const { currentUser, setCurrentUser } = useContext(UserContext);
+    const { setImageUrl, setRectangles } = useContext(ImageContext);
+    const nav = useNavigate();
     return (
         <Fragment>
             <div>
             {
-                isSignedIn ? 
+                currentUser ? 
                 <nav style={{ display: 'flex', justifyContent: 'flex-end' }}>
-                    <NavigationButton caption={"Sign Out"} />
+                    <NavigationButton caption={"Sign Out"} 
+                        onclick={ () => { 
+                            setCurrentUser(null);
+                            setImageUrl(null);
+                            setRectangles([]);
+                            nav('/signin');
+                        }}
+                    />
                 </nav>
                 :
                 <nav style={{ display: 'flex', justifyContent: 'flex-end' }}>
-                    <NavigationButton caption={"Sign In"} />
-                    <NavigationButton caption={"Register"} />
+                    <NavigationButton caption={"Sign In"} 
+                        onclick={ () => { nav('/signin')}}
+                    />
+                    <NavigationButton caption={"Register"} 
+                        onclick={ () => { nav('/register') } }
+                    />
                 </nav>
             }
             </div>
