81964d9d456cf0ae7c2d40edcc9962b221f932b4
Marc Edison Estaca <mebestaca@gmail.com>
2023-12-07 02:58:44 -0700

    deployed app2

--- src/index.js
+++ src/index.js
@@ -6,14 +6,17 @@ import App2 from './App2';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
 import { BrowserRouter } from 'react-router-dom';
-import { UserProvider } from './context/user.context';
+import { UserProvider } from './context/user/user.context';
+import { ImageProvider } from './context/image.context.jsx/image.context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <App2 />
+        <ImageProvider>
+          <App2 />
+        </ImageProvider>
       </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
