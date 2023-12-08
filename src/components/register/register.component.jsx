2fede7ddaada2366c443e03a5c859e410e6df1a0
Marc Edison Estaca <mebestaca@gmail.com>
2023-12-08 00:13:10 -0700

    converted register component to jsx

--- src/components/register/register.component.jsx
+++ src/components/register/register.component.jsx
@@ -1,7 +1,61 @@
 import { Fragment } from 'react';
 import Logo from '../logo/Logo';
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import { useContext } from 'react';
+import { UserContext } from '../../context/user/user.context';
 
 const Register = () => {
+
+    const nav = useNavigate();
+    const initialState = {
+        username: '',
+        email: '',
+        password: '',
+    }
+    const {setCurrentUser} = useContext(UserContext);
+    const [user, setUser] = useState(initialState);
+
+    const onUserChange = (event) => {
+        setUser(prevState => ({
+            ...prevState,
+            username: event.target.value,
+        }));
+    }
+
+    const onEmailChange = (event) => {
+        setUser(prevState => ({
+            ...prevState,
+            email: event.target.value,
+        }));
+    }
+
+    const onPasswordChange = (event) => {
+        setUser(prevState => ({
+            ...prevState,
+            password: event.target.value,
+        }));
+    }
+
+    const onRegister = () => {
+        fetch(`http://localhost:3000/register`, {
+            method: 'post',
+            headers: { 'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                email: user.email,
+                password: user.password,
+                name: user.username
+            })
+        })
+        .then(response => response.json()
+        .then(user => {
+            if (user) {
+                setCurrentUser(user);
+                nav('/');
+            }
+        })).catch(err => console.log(err));
+    }
+
     return( 
         <Fragment>
             <Logo/>
@@ -16,7 +70,7 @@ const Register = () => {
                             placeholder='username'
                             name='username'
                             id='username'
-                            onChange={ null }
+                            onChange={ onUserChange }
                         >
                         </input>
                     </div>
@@ -28,7 +82,7 @@ const Register = () => {
                             placeholder='sample@email.com'
                             name='email'
                             id='email'
-                            onChange={ null }
+                            onChange={ onEmailChange }
                         >
                         </input>
                     </div>
@@ -40,20 +94,20 @@ const Register = () => {
                             placeholder='password'
                             name='password'
                             id='password'
-                            onChange={ null }
+                            onChange={ onPasswordChange }
                         >
                         </input>
                     </div>
                     <div className='tc bt'>
                         <button 
                             className='mt2 white bg-dark-green grow ph3 pv2 pointer f4'
-                            onClick={ null }
+                            onClick={ onRegister }
                         >
                             Register
                         </button>
                         <p 
                             className='link dim grow f4 pointer'
-                            onClick={ null }
+                            onClick={ () => { nav('/signin') } }
                         >
                             Sign In
                         </p>
