0ce88c3b7586476d6f39b207c1e5c442acd3ac9a
Marc Edison Estaca <mebestaca@gmail.com>
2023-12-07 17:02:07 -0700

    added private route component

--- src/components/private-route/private-route.component.jsx
+++ src/components/private-route/private-route.component.jsx
@@ -1,9 +1,10 @@
 import { Navigate } from "react-router-dom";
+import { useContext } from "react";
+import { UserContext } from "../../context/user/user.context";
 
 const PrivateRoute = ({ children }) => {
-
-    const isLoggedIn = false;
-    return isLoggedIn ? children : <Navigate to='/signin' />
+    const {currentUser} = useContext(UserContext);
+    return currentUser ? children : <Navigate to='/signin' />
 }
 
 export default PrivateRoute;
