2be45d70095851539acdda36e8f6c139e8bca4c3
Marc Edison Estaca <mebestaca@gmail.com>
2023-12-14 20:36:16 -0700

    implemented error component

--- src/components/register/register.component.jsx
+++ src/components/register/register.component.jsx
@@ -1,21 +1,21 @@
-import { Fragment } from 'react';
-import Logo from '../logo/logo.component';
 import { useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 import { useContext } from 'react';
 import { UserContext } from '../../context/user/user.context';
+import Logo from '../logo/logo.component';
 import FormInput from '../form-input/form-input.component';
+import ErrorMessage from '../error/error.component';
 
 const Register = () => {
-
     const nav = useNavigate();
     const initialState = {
         username: '',
         email: '',
         password: '',
     }
-    const {setCurrentUser} = useContext(UserContext);
     const [user, setUser] = useState(initialState);
+    const {setCurrentUser} = useContext(UserContext);
+    const [errorList, setErrorList] = useState([]);
 
     const onUserChange = (event) => {
         setUser(prevState => ({
@@ -39,6 +39,37 @@ const Register = () => {
     }
 
     const onRegister = () => {
+        const errors = [];
+
+        if (!user.username) {
+            errors.push("Username cannot be null");
+        }
+
+        if (user.username.length < 6) {
+            errors.push("Username must be longer than 6 characters");
+        }
+
+        if (user.username.length > 100) {
+            errors.push("Username must not exceed 100 characters");
+        }
+
+        if (!user.email) {
+            errors.push("Email cannot be null");
+        }
+
+        if (!user.email.includes('@')) {
+            errors.push("Incorrect email format");
+        }
+
+        if (!user.password) {
+            errors.push("Password cannot be null");
+        }
+
+        if (errors.length > 0) {
+            setErrorList(errors);
+            return;
+        }
+
         fetch(`http://localhost:3000/register`, {
             method: 'post',
             headers: { 'Content-Type': 'application/json'},
@@ -58,11 +89,16 @@ const Register = () => {
     }
 
     return( 
-        <Fragment>
+        <>
             <Logo/>
             <div className='bg-white signup-box center shadow-3 br3 mt4 pv3 bg-washed-green' style={{ width: '400px' }}>
                 <div className='mh3'>
                     <h1 className='tc'>Register</h1>
+
+                    {
+                        errorList.length > 0 ? <ErrorMessage errorMessages={errorList} /> : <></>
+                    }
+                    
                     <FormInput
                         label= "Username:"
                         inputOptions={{
@@ -70,6 +106,9 @@ const Register = () => {
                             type: 'text', 
                             name: 'username',
                             id: 'username',
+                            required: true,
+                            minLength: '6',
+                            maxLength: '100',
                             onChange:onUserChange
                         }}
                     />
@@ -80,6 +119,8 @@ const Register = () => {
                             type:'email', 
                             name:'email',
                             id:'email',
+                            minLength: '3',
+                            required: true,
                             onChange:onEmailChange
                         }}
                     />
@@ -90,6 +131,8 @@ const Register = () => {
                             type: 'password',
                             name: 'password',
                             id: 'password',
+                            minLength: '6',
+                            required: true,
                             onChange: onPasswordChange
                         }}
                     />
@@ -109,7 +152,7 @@ const Register = () => {
                     </div>
                 </div>
             </div>
-        </Fragment>
+        </>
     );
 }
 
