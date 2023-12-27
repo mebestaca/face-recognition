a91874de0b4e67ca32031dc0fad0d4b86c50166c
Marc Edison Estaca <mebestaca@gmail.com>
2023-12-26 22:05:09 -0700

    migrated the api code to back end

--- src/components/itemlinkform/item-link-form.component.jsx
+++ src/components/itemlinkform/item-link-form.component.jsx
@@ -5,10 +5,11 @@ import { UserContext } from '../../context/user/user.context';
 
 const ItemLinkForm = () => {
 
-    const { setImageUrl, imageUrl, setRectangles } = useContext(ImageContext);
+    const { imageUrl, setImageUrl,  setRectangles } = useContext(ImageContext);
     const { currentUser, setCurrentUser } = useContext(UserContext);
 
     const onInputChange = (event) => {
+        setRectangles([]);
         setImageUrl(event.target.value);
     }
 
@@ -56,59 +57,16 @@ const ItemLinkForm = () => {
     }
     
     const detectImage = () => {
-        ///////////////////////////////////////////////////////////////////////////////////////////////////
-        // In this section, we set the user authentication, user and app ID, model details, and the URL
-        // of the image we want as an input. Change these strings to run your own example.
-        //////////////////////////////////////////////////////////////////////////////////////////////////
-
-        // Your PAT (Personal Access Token) can be found in the portal under Authentification
-        const PAT = '9a1c86d08e8841ebae2d7571f88f058b';
-        // Specify the correct user_id/app_id pairings
-        // Since you're making inferences outside your app's scope
-        const USER_ID = 'mebestaca';       
-        const APP_ID = 'face-detection';
-        // Change these to whatever model and image URL you want to use
-        const MODEL_ID = 'face-detection';
-        const MODEL_VERSION_ID = '6dc7e46bc9124c5c8824be4822abe105';  
-        const IMAGE_URL = imageUrl;
-
-        ///////////////////////////////////////////////////////////////////////////////////
-        // YOU DO NOT NEED TO CHANGE ANYTHING BELOW THIS LINE TO RUN THIS EXAMPLE
-        ///////////////////////////////////////////////////////////////////////////////////
-
-        const raw = JSON.stringify({
-            "user_app_id": {
-                "user_id": USER_ID,
-                "app_id": APP_ID
-            },
-            "inputs": [
-                {
-                    "data": {
-                        "image": {
-                            "url": IMAGE_URL
-                        }
-                    }
-                }
-            ]
-        });
-
-        const requestOptions = {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Authorization': 'Key ' + PAT
-            },
-            body: raw
-        };
-
-        // NOTE: MODEL_VERSION_ID is optional, you can also call prediction with the MODEL_ID only
-        // https://api.clarifai.com/v2/models/{YOUR_MODEL_ID}/outputs
-        // this will default to the latest version_id
-
-        fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/versions/" + MODEL_VERSION_ID + "/outputs", requestOptions)
-            .then(response => response.json())
-            .then(result => drawRectangle(getRectangle(result)))
-            .catch(error => console.log('error', error));
+        fetch(`http://localhost:3000/imagedetect`, {
+            method: 'post',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                imageUrl: imageUrl
+            })
+        })
+        .then(response => response.json())
+        .then(result =>  drawRectangle(getRectangle(result))) 
+        .catch(console.log);
     }
 
     return (
@@ -121,6 +79,7 @@ const ItemLinkForm = () => {
                         className='pa2 f4 w-70' 
                         type='text' 
                         placeholder='url'
+                        maxLength={200}
                         onChange={ onInputChange }/>
                     <button 
                         className='white bg-dark-green w-30 grow ph3 pv2 pointer f4'
